b77a2631d8d04aa1dd0725be3fccd2e26e168861
Nursultan <omurgazievnur@gmail.com>
2024-01-16 16:59:39 +0600

    new file:   css/style.css 	new file:   img/contact-form/01.jpg 	new file:   img/header/logo.svg 	new file:   img/projects/01.svg 	new file:   img/projects/02.svg 	new file:   img/projects/03.svg 	new file:   img/services/01.svg 	new file:   img/welcome/01.png 	new file:   index.html 	new file:   js/script.js 	new file:   prepros.config

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+"use strict"
+
+
+const burgerButton = document.querySelector('.header__button');
+
+if (burgerButton) {
+    const menu = document.querySelector('.header__menu');
+    burgerButton.addEventListener("click", function (e) {
+        burgerButton.classList.toggle('_active');
+        menu.classList.toggle('_active');
+        document.body.classList.toggle('_lock');
+    });
+}
+
